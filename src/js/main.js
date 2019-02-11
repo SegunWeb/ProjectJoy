083cb4f401d71c12553bb5648304f44c99a138d5
Sergey <mrsegun2014@gmail.com>
2019-02-11 18:55:02 +0200

    add new style and script img

--- src/js/main.js
+++ src/js/main.js
@@ -21,9 +21,45 @@ $('.testimonials-slider').slick({
 });
 
 //--------pop-up------
+
 $('.btn__auth').click( function () {
     $('.aunt').show()
 });
 $('.close').click( function () {
     $('.aunt').hide()
 });
+
+// ----- bg img video -----
+$(document).on('click', '.img-video', function() {
+    var $video = $(this).next(),
+        src = $video.attr('data-src');
+    $video.attr('src', src + '&autoplay=1');
+    $video.addClass('d-block');
+    var _self = this;
+    $(_self).addClass('d-none');
+    var _btn = $(this).next().next();
+    $(_btn).addClass('d-none');
+
+    if ($video.attr('src', src + '&autoplay=1')) {
+        $('.img-video, .play-video').click(function () {
+            ($video.attr('src', src + '&autoplay=0'))
+        })
+    }
+
+});
+$(document).on('click', '.play-video', function() {
+    var $video = $(this).prev(),
+        src = $video.attr('data-src');
+    $video.attr('src', src + '&autoplay=1');
+    $video.addClass('d-block');
+    var _self = this;
+    $(_self).addClass('d-none');
+    var _btn = $(this).prev().prev();
+    $(_btn).addClass('d-none');
+
+    if ($video.attr('src', src + '&autoplay=1')) {
+        $('.play-video, .img-video').click(function () {
+            ($video.attr('src', src + '&autoplay=0'))
+        })
+    }
+})
